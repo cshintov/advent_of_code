cd579cfc4e068c517fa551f3b76ab89ef7fb0bc5
cshintov@gmail.com <shinto@obmondo.com>
2021-01-03 17:57:31 +0530

    optimize day 9

--- day_9/solution.py
+++ day_9/solution.py
@@ -20,12 +20,6 @@ def find_invalid_code(codes, preamble_length):
             return num
     return None
 
-def find_contiguous_components_brute_force(codes, num):
-    def helper(n):
-        for i, _ in enumerate(codes):
-            if sum(codes[i:i+n]) == num:
-                return codes[i:i+n]
-        return []
 
     for i in range(2, len(codes)):
         components = helper(i)
@@ -35,27 +29,36 @@ def find_contiguous_components_brute_force(codes, num):
     return components
 
 def find_contiguous_components(codes, num):
-    def look_back(i):
-        cur_sum = 0
-        for j in range(i, 0, -1):
-            cur_sum += codes[j]
-            if cur_sum == num:
-                return j
-        return None
+    """ Find contiguous subarray which adds apt to num. """
 
-    for i, code in enumerate(codes):
-        start = look_back(i)
-        if not start is None:
-            return codes[start:i+1]
+    def watch1():
+        print(f'{cur_sum=} : {i=} : {codes[i]=} : {start=} : {cur_sum > num = }')
+    def watch2():
+        print(f'\n{cur_sum=} : {codes[start]=};{cur_sum - codes[start] =} : {cur_sum > num = }')
+
+    cur_sum = codes[0]
+    start = end = 0
+    for i in range(1, len(codes)):
+        # watch1()
+        if cur_sum == num:
+            return codes[start : i]
+        cur_sum = cur_sum + codes[i]
+        if cur_sum > num:
+            while cur_sum > num:
+                # watch2()
+                cur_sum -= codes[start]
+                start += 1
     return []
-        
 
 if __name__ == '__main__':
-    codes = list(read('input.txt'))
+    codes = list(read('test.txt'))
+    codes = [45, 71, 73, 26, 41, 5, 54, 23, 35, 57, 22, 75, 21, 72, 77]
+    assert find_contiguous_components(codes, sum([75,21,72])) == [75,21,72]
 
-    solution_1 = find_invalid_code(codes, 25)
-    print(f'Part 1 solution: {solution_1}')
+    # solution_1 = find_invalid_code(codes, 25)
+    # print(f'Part 1 solution: {solution_1}')
 
-    solution_2 = find_contiguous_components(codes, solution_1)
-    print(solution_2)
-    print(f'Part 2 solution: {min(solution_2) + max(solution_2)}')
+    # solution_1 = 23278925
+    # solution_2 = find_contiguous_components(codes, solution_1)
+    # print(solution_2)
+    # print(f'Part 2 solution: {min(solution_2) + max(solution_2)}')
